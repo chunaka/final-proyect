23a9398f24c69eff61d63e8e6b3bbe33a8d09e64
chunko <castromontoyajuancamilo@gmail.com>
2025-11-10 21:09:00 -0500

    refactor: Simplify PCB structure and move logic to Process class

--- models/pcb.py
+++ models/pcb.py
@@ -43,42 +43,6 @@ class PCB:
         self.response_time = -1
         self.completion_time = 0
         self.start_time = -1
-    
-
-    def update_state(self, new_state:ProcessState, current_time:int=0) -> None:
-        """
-        Update process state
-        """
-        self.state = new_state
-        if new_state == ProcessState.RUNNING and self.start_time == -1 and current_time != 0:
-            self.start_time = current_time
-    
-    
-    def increment_cpu_time(self, time_units:int) -> None:
-        """ 
-        Simulate CPU usage
-        """
-
-        self.remaining_time = max(0, self.remaining_time - time_units)
-        self.program_counter += time_units
-
-    def is_completed(self) -> bool:
-        """
-        Check if process has finished
-        """
-        return self.remaining_time <= 0 
-
-    def calculate_metrics(self, current_time:int) -> None:
-        """
-        Calculate waiting and turnaround times
-        """
-        if self.state == ProcessState.TERMINATED:
-            self.completion_time = current_time
-            self.turnaround_time = self.completion_time - self.arrival_time
-            self.waiting_time = self.turnaround_time - self.burst_time
-            
-            if self.response_time == -1 and self.start_time != -1:
-                self.response_time = self.start_time - self.arrival_time
-    
+        
     def __str__(self):
-        return (f"PCB(PID={self.pid}, State={self.state.value}, Remaining={self.remaining_time}, Priority={self.priority})")
+        return (f"PCB(PID={self.pid}, User = {self.user}, State={self.state.value}, Remaining={self.remaining_time}, Priority={self.priority})")
